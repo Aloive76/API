8c961bbcf84edf0b345678435d644d465868297a
egonixaimgod <bodnarbence2002@gmail.com>
2023-03-02 20:16:30 +0100

    Helyben minden működik. REST API

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 const gyumolcsok = [
-    { name: 'szilva', quantity: 35, price: 8 },
-    { name: 'alma', quantity: 45, price: 8.3 },
-    { name: 'körte', quantity: 25, price: 9.5 },
-    { name: 'barack', quantity: 37, price: 12 }
+    { id: 1, name: 'szilva', quantity: 35, price: 8 },
+    { id: 2, name: 'alma', quantity: 45, price: 8.3 },
+    { id: 3, name: 'körte', quantity: 25, price: 9.5 },
+    { id: 4, name: 'barack', quantity: 37, price: 12 }
   ];
-  const tbody =document.querySelector('#tbody');
+  const tbody = document.querySelector("#tbody");
   
   function generateTbody() {
     gyumolcsok.forEach((gyumolcs) => {
       
-
         let tr = document.createElement('tr');
         let tdName = document.createElement('td');
         let tdQuantity = document.createElement('td');
@@ -23,19 +22,33 @@ const gyumolcsok = [
         tr.append(tdName);
         tr.append(tdQuantity);
         tr.append(tdPrice);
-        tr.append(generateTdDelete());
+        tr.append(generateTdDelete(gyumolcs.id));
     });
   }
   generateTbody();
 
-  function generateTdDelete() {
+  function generateTdDelete(id) {
     let td = document.createElement('td');
     let button = document.createElement('button');
     button.textContent = "Törlés";
     button.classList = "btn btn-warning";
     button.addEventListener('click', () => {
-        console.log("működik");
+        console.log(id);
+        let index = 0;
+        let count = 0;
+        gyumolcsok.forEach((gy) => {
+            if (gy.id == id) {
+                index = count;
+        }
+        count++
+    })
+
+        console.log(index);
+        gyumolcsok.splice(index, 1);
+        tbody
+        generateTbody
     });  
+    
     td.append(button);
     return td;
   }
